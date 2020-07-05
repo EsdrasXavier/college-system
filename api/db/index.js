780177b00df6168eed977699f0e399e337cdd73c
Esdras Xavier <esdrasxa@gmail.com>
2020-07-05 04:57:34 -0300

    Arrumado para rodar no heroku.

--- api/db/index.js
+++ api/db/index.js
@@ -1,24 +1,50 @@
-const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
-
 const Person = require('../models/Person');
 const Address = require('../models/Address');
 const Registration = require('../models/Registration');
 const Authority = require('../models/Authority');
 const Billing = require('../models/Billing');
 
-const connection = new Sequelize(dbConfig);
+if (!global.hasOwnProperty('db')) {
+  var Sequelize = require('sequelize')
+    , sequelize = null
 
-Person.init(connection);
-Address.init(connection);
-Registration.init(connection);
-Authority.init(connection);
-Billing.init(connection);
+  if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      port: match[4],
+      host: match[3],
+      logging: true //false
+    })
+  } else {
+    // the application is executed on the local machine ... use mysql
+    sequelize = new Sequelize(dbConfig);
+  }
 
-Person.associate(connection.models);
-Address.associate(connection.models);
-Registration.associate(connection.models);
-Authority.associate(connection.models);
-Billing.associate(connection.models);
+  Person.init(sequelize);
+  Address.init(sequelize);
+  Registration.init(sequelize);
+  Authority.init(sequelize);
+  Billing.init(sequelize);
 
-module.exports = connection;
+  Person.associate(sequelize.models);
+  Address.associate(sequelize.models);
+  Registration.associate(sequelize.models);
+  Authority.associate(sequelize.models);
+  Billing.associate(sequelize.models);
+
+  global.db = {
+    Sequelize: Sequelize,
+    sequelize: sequelize
+    // add your other models here
+  }
+
+  /*
+    Associations can be defined here. E.g. like this:
+    global.db.User.hasMany(global.db.SomethingElse)
+  */
+}
+
+module.exports = global.db
--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const routes = require('./api/routes');
 const auth = require("./api/controllers/auth.js")();
-
-require('./api/db');
-
+const db = require('./api/db/index');
+const http = require('http');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(auth.initialize());
 app.use(express.json());
 app.use(routes);
-app.listen(port);
+// app.listen(port);
+app.set('port', port);
 
-console.log('API server started on: ' + port);
+db.sequelize.sync().then(function () {
+  http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+  });
+});
